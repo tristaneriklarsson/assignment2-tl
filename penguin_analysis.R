9f3027f9425631dc31572a7f853dca9c2e1a0356
cwilmot7 <cwilmot@wesleyan.edu>
2025-10-15 09:41:48 -0400

    Added my code

--- penguin_analysis.R
+++ penguin_analysis.R
@@ -20,11 +20,3 @@ ggplot(penguins, aes(x = flipper_length_mm, y = body_mass_g, color = species)) +
   theme_minimal() +
   labs(title = "Flipper Length vs. Body Mass by Species")
 ggsave("flipper_vs_mass.png")
-
-sumstats <- penguins %>%
-  group_by(island) %>%
-  summarise(
-    mean_body_mass = mean(body_mass_g, na.rm = TRUE),
-    mean_bill_length = mean(bill_length_mm, na.rm = TRUE)
-  )
-write.csv(sumstats, "secondsummary.csv", row.names = FALSE)
